7361281fbad0aaa85a8800be3029d84cedf23b0d
codingwolf-at <atameshwari9@gmail.com>
2021-08-05 11:40:13 +0530

    FINISHED: intermediate , 001

--- 001 - Operator, Branching, Loops/intermediate.js
+++ 001 - Operator, Branching, Loops/intermediate.js
@@ -57,4 +57,43 @@ const printFibonacci = num => {
   return resultArr;
 }
 
-console.log(printFibonacci(9));
+// Q5. Write a program to take an input from a user and find its Factorial. Example: Factorial of 5 is 120
+
+const printFactorial = num => {
+  if (num < 1) {
+    return 1
+  } else {
+    return num*printFactorial(num-1);
+  }
+}
+
+// Q6. Write a Program to take a number input from user and find if the number is Prime or not.
+
+const checkPrime = num => {
+  if (num > 1) {
+    for (let i = 2; i<num; i++) {
+      if (num%i === 0) {
+        return false
+      } else {
+        return true
+      }
+    }
+  } else {
+    return false
+  }
+}
+
+// console.log(checkPrime(9));
+// -> true // why? 
+
+// Q7. Write a program to take a day as an input and determine whether it is a weekday or weekend. Example: Tuesday is weekday.
+
+const checkDay = day => {
+  if (day[0].toLowerCase() === "s") {
+    return "Weekend"
+  } else {
+    return "Weekday"
+  }
+} 
+
+console.log()
